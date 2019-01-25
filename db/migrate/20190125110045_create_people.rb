63ed078b83b64d0e0e497c5738eb2d57c76431c7
Devesh Lamichhane <deveshl@protonmail.com>
2019-01-25 16:52:44 +0545

    initial commit

---
+++ db/migrate/20190125110045_create_people.rb
@@ -0,0 +1,11 @@
+class CreatePeople < ActiveRecord::Migration[5.2]
+  def change
+    create_table :people do |t|
+      t.string :firstName
+      t.string :lastName
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
